f5bdf4169e4af59931224438e5f38164c77bd01d
engineeredtoimperfection <207467614+engineeredtoimperfection@users.noreply.github.com>
2025-07-23 02:17:17 +0530

    Display a loading cursor for a sec upon link clicks

--- script.js
+++ script.js
@@ -5,6 +5,27 @@ console.log(`You've visited this site ${visits} times.`)
 
 // Restrict this value between 1 and 8 (inclusive)
 const visitCountCapped = Math.min(Math.max(visits, 1), 8);
-const animationMultiplier = 1 - (( visitCountCapped - 1) * 0.1 );
+const animationMultiplier = 1 - ((visitCountCapped - 1) * 0.1);
 
 document.documentElement.style.setProperty("--animation-speed-multiplier", animationMultiplier);
+
+const anchorTags = document.getElementsByTagName("a");
+
+for (const anchorTag of anchorTags) {
+    anchorTag.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        const targetAnchorObject = e.currentTarget;
+        const href = targetAnchorObject.href;
+        const rel = targetAnchorObject.rel;
+
+        const relCsv = rel.split(" ").join(",");
+
+        targetAnchorObject.style.cursor = 'wait';
+
+        setTimeout(() => {
+            targetAnchorObject.style.cursor = '';
+            window.open(href, "_target", relCsv);
+        }, 1000);
+    })
+}
