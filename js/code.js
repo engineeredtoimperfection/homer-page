c29becdb657898d5c4b4608c065aac72ffe7a079
engineeredtoimperfection <207467614+engineeredtoimperfection@users.noreply.github.com>
2025-07-26 10:45:07 +0530

    Move activity data to a json config

---
+++ js/code.js
@@ -0,0 +1,18 @@
+fetch('../config.json')
+    .then(res => res.json())
+    .then(config => {
+
+        const code = config.code.activity
+
+        const play = document.getElementById('play-activity')
+        const practice = document.getElementById('practice-activity')
+        const lastUpdated = document.getElementById('last-updated')
+
+        play.textContent = code.play
+        practice.textContent = code.practice
+        lastUpdated.textContent = `Last updated: ${code.last_updated}`
+        
+    })
+    .catch(error => {
+        console.error('Error fetching config:', error)
+    })
