66becdeefe05d4291bfc778f84a13b8a1ac0e0d2
engineeredtoimperfection <207467614+engineeredtoimperfection@users.noreply.github.com>
2025-08-20 09:00:00 +0530

    Create a music page to cover everything music

---
+++ js/music.js
@@ -0,0 +1,15 @@
+const guitarTile = document.getElementById('guitar-tile')
+const vocalsTile = document.getElementById('vocals-tile')
+const musicianshipTile = document.getElementById('musicianship-tile')
+
+guitarTile.addEventListener('click', () => {
+    window.open('https://the-practice-page.vercel.app')
+})
+
+vocalsTile.addEventListener('click', () => {
+    window.open('../pages/vocals.html')
+})
+
+musicianshipTile.addEventListener('click', () => {
+    
+})
