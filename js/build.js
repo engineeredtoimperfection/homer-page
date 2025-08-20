ceb3904cbdb851f0c4be89db41b740acd3f52d6c
engineeredtoimperfection <207467614+engineeredtoimperfection@users.noreply.github.com>
2025-08-20 15:46:02 +0530

    Create a build page to cover both code and design

---
+++ js/build.js
@@ -0,0 +1,10 @@
+const codeTile = document.getElementById('code-tile')
+const designTile = document.getElementById('design-tile')
+
+codeTile.addEventListener('click', () => {
+    window.open('../pages/code.html')
+})
+
+designTile.addEventListener('click', () => {
+    window.open('../pages/design.html')
+})
